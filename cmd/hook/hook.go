52260ef6ea770470de79ab8e6509e7f181606d7e
David Stark <david@starkers.org>
2020-02-21 18:21:31 +0000

    hook play

---
+++ cmd/hook/hook.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"os"
+)
+
+func handleHook(w http.ResponseWriter, r *http.Request) {
+	fmt.Printf("headers: %v\n", r.Header)
+	_, err := io.Copy(os.Stdout, r.Body)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+}
+
+func main() {
+	http.HandleFunc("/hook", handleHook)
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
